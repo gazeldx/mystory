79e3f6877523318f940a8dd31781b3897afc12f3
Zhang Jian <zjloveztt@gmail.com>
2011-11-28 12:26:22 +0800

    dsfdsdf

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,14 +1,8 @@
 #Member login
 class LoginController < ApplicationController
-  def to_login
-    respond_to do |format|
-      format.html
-    end
+  def to_login    
   end
 
   def login
-#    @user = User.where(["username = ?", params[:username]]).first
-#    session["user_id"]=@user.id
-#    redirect_to "/"+params[:username]
   end
 end
