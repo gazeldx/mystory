4faf770a04f0cd3f2421306b0e6b07360082465b
Zhang Jian <zjloveztt@gmail.com>
2012-02-08 13:41:24 +0800

    r

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,22 +8,6 @@ Cms::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
-  # Don't fallback to assets pipeline if a precompiled asset is missed.
-  #Modified by zhangjian because heroku show:ActionView::Template::Error (application.css isn't precompiled)
-  config.assets.compile = true
-
-  # Generate digests for assets URLs
-  config.assets.digest = true
-
-  # Defaults to Rails.root.join("public/assets")
-  # config.assets.manifest = YOUR_PATH
-
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
@@ -58,4 +42,26 @@ Cms::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+ # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = true
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed.
+  #Modified by zhangjian because heroku show:ActionView::Template::Error (application.css isn't precompiled)
+  config.assets.compile = true
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
+  # Defaults to Rails.root.join("public/assets")
+  # config.assets.manifest = YOUR_PATH
+  
+#  config.assets.js_compressor  = :uglifier
+#  config.assets.css_compressor = :scss
+#  config.assets.precompile += %w(application.css application.js topics.css topics.js window.css front.css cpanel.css search.css
+#  users.css posts.css posts.js pages.css pages.js notifications.js notifications.css sites.css sites.js
+#  gfdynamicfeedcontrol.css gfdynamicfeedcontrol.js mobile.css)
 end
