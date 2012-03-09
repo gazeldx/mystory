2a93e9b167616c10d269c26d1bbcff14c01120f5
Zhang Jian <zjloveztt@gmail.com>
2012-03-09 17:09:33 +0800

    env2

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Cms::Application.initialize!
 
-DOMAIN_NAME = "mystory2.cc"
+#DOMAIN_NAME = "mystory2.cc"
+DOMAIN_NAME = "mystory.cc"
 #SITE_URL = "http://" + DOMAIN_NAME + ":8080"
 SITE_URL = "http://" + DOMAIN_NAME
 
