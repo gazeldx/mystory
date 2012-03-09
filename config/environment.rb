17d719698da698fff07ec4e7b7ffebf3e705a9fa
Zhang Jian <zjloveztt@gmail.com>
2012-03-09 17:35:37 +0800

    env8

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,9 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Cms::Application.initialize!
 
-#DOMAIN_NAME = "mystory2.cc"
-DOMAIN_NAME = "mystory.cc"
-#SITE_URL = "http://" + DOMAIN_NAME + ":8080"
-SITE_URL = "http://" + DOMAIN_NAME
+#DOMAIN_NAME = "mystory.cc"
+#SITE_URL = "http://" + DOMAIN_NAME
+DOMAIN_NAME = "mystory2.cc"
+SITE_URL = "http://" + DOMAIN_NAME + ":8080"
 
 #Slim::Engine.set_default_options :sections => true
