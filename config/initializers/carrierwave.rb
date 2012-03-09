78a1512cc048c6cb0e96633a5439ee0e68ee7613
Zhang Jian <zjloveztt@gmail.com>
2012-03-09 17:13:38 +0800

    env3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,6 +2,6 @@ CarrierWave.configure do |config|
   config.storage = :upyun
   config.upyun_username = "gazeldx"
   config.upyun_password = 'zj85918825'
-  config.upyun_bucket = "mystorytest"
-  config.upyun_bucket_domain = "mystorytest.b0.upaiyun.com"
+  config.upyun_bucket = "mystory"
+  config.upyun_bucket_domain = "mystory.b0.upaiyun.com"
 end
