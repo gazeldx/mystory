ec3e71067751fcc9416d6281afea4ed057987c01
Zhang Jian <zjloveztt@gmail.com>
2012-03-09 17:06:20 +0800

    env

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require File.expand_path('../application', __FILE__)
 Cms::Application.initialize!
 
 DOMAIN_NAME = "mystory2.cc"
-SITE_URL = "http://" + DOMAIN_NAME + ":8080"
-#SITE_URL = "http://" + DOMAIN_NAME
+#SITE_URL = "http://" + DOMAIN_NAME + ":8080"
+SITE_URL = "http://" + DOMAIN_NAME
 
 #Slim::Engine.set_default_options :sections => true
