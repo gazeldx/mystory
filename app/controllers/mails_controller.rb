5ed28cc61361d4e5c969c385bfba2af897c8e93d
Zhang Jian <zjloveztt@gmail.com>
2012-08-18 22:40:54 +0800

    08182

--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -3,7 +3,7 @@ class MailsController < ApplicationController
   layout 'help'
   
   def new_function_email
-    @users = User.where(["id >= ? AND id <= ?", params[:start], params[:end]]).order("id DESC")
+    get_users
     #      @users = User.find([13, 14])
     puts @users.inspect
     @users.each do |user|
@@ -16,5 +16,24 @@ class MailsController < ApplicationController
         logger.info("Email sent to #{user.email}")
       end
     end
+  end
+
+  def note_21days_email
+    get_users
+    #      @users = User.find([13, 14])
+    puts @users.inspect
+    @users.each do |user|
+      if user.email.match(/.*mystory\.cc/)
+        puts ">>>>>>>>>>>>>>>>not send #{user.email}"
+      else
+        UserMailer.note_21days(user).deliver
+        logger.info("Email sent to #{user.email} from new_function_email")
+      end
+    end
+  end
+
+  private
+  def get_users
+    @users = User.where(["id >= ? AND id <= ?", params[:start], params[:end]]).order("id DESC")
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,13 @@ class UserMailer < ActionMailer::Base
 #  default from: "mail_zlj@163.com"
   default from: "zjloveztt@gmail.com"
 
+  def note_21days(user)
+    @user = user
+    puts "--begin mail sending at note_21days(#{@user.email})...."
+    mail(:to => @user.email, :subject => t('mail.subject.note_21days', w: @user.name))
+    puts "--end mail sent"
+  end
+  
   def new_function(user)
     @user = user
     puts "--begin mail sending at new_function(#{@user.email})...."
